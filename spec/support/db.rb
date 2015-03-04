94d992361eb48fc5d400b4710bd72fa9529bbb55
Vladimir Kochnev <hashtable@yandex.ru>
2015-03-04 19:02:39 +0300

    disable debug stdout for AR

--- spec/support/db.rb
+++ spec/support/db.rb
@@ -5,5 +5,5 @@ ActiveRecord::Base.establish_connection adapter: 'postgresql',
                                         database: 'acts_as_ltree_test',
                                         min_messages: 'warning'
 
-ActiveRecord::Base.logger = Logger.new(STDOUT)
-ActiveRecord::Base.logger.level = Logger::DEBUG
+#ActiveRecord::Base.logger = Logger.new(STDOUT)
+#ActiveRecord::Base.logger.level = Logger::DEBUG
