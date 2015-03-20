30e4c4920e7bca03f479309a49439bac2c0333e7
Vladimir Kochnev <hashtable@yandex.ru>
2015-03-20 12:29:00 +0300

    finish up class methods

--- lib/acts_as_ltree.rb
+++ lib/acts_as_ltree.rb
@@ -7,26 +7,28 @@ module ActsAsLtree
   module ClassMethods
     def acts_as_ltree(opts = {})
       column_name = opts[:on] || :path
-      query_builder = QueryBuilder.new(arel_table[column_name])
-      base_options = {
-        column_name: column_name
-      }
 
-      define_singleton_method :descendants_of do |path, options = {}|
-        where query_builder.descendants(path, options)
-      end
+      instance_eval <<-CLASS_METHODS
+        def ltree_query_builder
+          @query_builder ||= ActsAsLtree::QueryBuilder.new(arel_table[:#{column_name}])
+        end
 
-      define_singleton_method :ancestors_of do |path|
-        where query_builder.ancestors(path)
-      end
+        def descendants_of(path, options={})
+          where ltree_query_builder.descendants(path, options)
+        end
 
-      define_singleton_method :matching_lquery do |query|
-        where query_builder.matching_lquery(query)
-      end
+        def ancestors_of(path)
+          where ltree_query_builder.ancestors(path)
+        end
 
-      define_singleton_method :matching_ltxtquery do |query|
-        where query_builder.matching_ltxtquery(query)
-      end
+        def matching_lquery(query)
+          where ltree_query_builder.matching_lquery(query)
+        end
+
+        def matching_ltxtquery(query)
+          where ltree_query_builder.matching_ltxtquery(query)
+        end
+      CLASS_METHODS
 
       class_eval <<-INSTANCE_METHODS
 
@@ -50,7 +52,7 @@ module ActsAsLtree
         end
 
         def preload_descendants(options = {})
-          SubtreeCache.new(self, #{base_options}.merge(options)).proxify(self)
+          SubtreeCache.new(self, {column_name: :#{column_name}}.merge(options)).proxify(self)
         end
 
         def depth
@@ -70,7 +72,6 @@ module ActsAsLtree
         end
 
       INSTANCE_METHODS
-
     end
   end
 end
